ff5d9689b4d69bdf64f9ea981d9f29328ecaa989
maxx1559 <55869522+maxx1559@users.noreply.github.com>
2022-04-26 13:08:46 +0200

    Menu implementation
    Uses TMPro, so imported that. Uses big cartesian_coordinates.exe file, so check google drive for that

---
+++ Bachelor-Unity/Assets/Script/OptionsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public static bool depthFilter = false;
+    public static bool axisFilter = false;
+    public static bool nearestNeighbour = false;
+    public static bool outlierHeight = false;
+    public static int minDepth = 0;
+    public static int maxDepth = 10;
+
+    public static bool heightMap = true;
+
+    public GameObject toggleGroup;
+
+    
+
+    
+
+    
+}
---
+++ Bachelor-Unity/Assets/Script/StateNameController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateNameController : MonoBehaviour
+{
+    public static string SevenKPath;
+    public static string JsonPath;
+
+
+}
